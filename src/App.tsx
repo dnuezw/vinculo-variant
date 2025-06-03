ae787521bdcc51155e53e1b997ef9a934b39e600
dnuez <dnuezw@gmail.com>
2025-06-03 19:10:37 +0200

    add title

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import type { FC } from 'react'
 import './App.css'
 
 const App: FC = () => {
-  return <></>
+  return (
+    <main className='presentation'>
+      <h1>The game</h1>
+    </main>
+  );
 }
 
 export default App
