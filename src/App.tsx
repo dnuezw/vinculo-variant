cf6222d5da1ca350266b8044397cec06d6b3312b
dnuez <dnuezw@gmail.com>
2025-06-03 20:20:48 +0200

    add basic information and ctas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import type { FC } from 'react'
 import './App.css'
+import play from './assets/icons/play.svg'
 
 const App: FC = () => {
   return (
     <main className='presentation'>
-      <h1>The game</h1>
-      <p>Un juego to wapo hecho por el samu</p>
+      <div className='title'>
+        <h1>The game</h1>
+        <p>Un juego to wapo hecho por el samu</p>
+      </div>
+      <div className='actions'>
+        <button className='trailer'><img src={play} className='playIcon'/>Play trailer</button>
+        <button className='coming-soon'>coming soon</button>
+      </div>
     </main>
   );
 }
