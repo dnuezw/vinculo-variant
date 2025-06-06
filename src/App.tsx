123d4b4e52269160b0d1eb5b5a3670a194eb12c2
dnuez <dnuezw@gmail.com>
2025-06-06 18:11:47 +0200

    Added story section

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,30 @@ import './App.css'
 import play from './assets/icons/play.svg'
 import logo from './assets/images/logo.png'
 import logoText from './assets/images/logoText.png'
+import story from './assets/images/story.jpg'
 
 const App: FC = () => {
   return (
-    <main className='presentation'>
-      <div className='title'>
-        <div className='logo'>
-          <img className='icon' src={logo} />
-          <img className='text' src={logoText} />
+    <>
+      <main className='presentation'>
+        <div className='title'>
+          <div className='logo'>
+            <img className='icon' src={logo} />
+            <img className='text' src={logoText} />
+          </div>
+          <p className='subtitle'>One bufanda one destiny...</p>
         </div>
-        <p className='subtitle'>One bufanda one destiny...</p>
-      </div>
-      <div className='actions'>
-        <button className='trailer'><img src={play} className='playIcon'/>Play trailer</button>
-        <button className='coming-soon'>coming soon</button>
-      </div>
-    </main>
+        <div className='actions'>
+          <button className='trailer'><img src={play} className='playIcon'/>Play trailer</button>
+          <button className='coming-soon'>coming soon</button>
+        </div>
+      </main>
+      <section className='story'>
+        <p>
+          En una profunda cueva, donde el Sol es desconocido, el reino de las altas torres de mármol se sume en una cruenta guerra santa. La Vinculación, habilidad que permite a los habitantes del reino adoptar las propiedades de un objeto cercano a ellos durante su nacimiento, y los límites éticos de esta se encuentran en el centro del conflicto. Un ciudadano de baja alcurnia descubre en el caos una antigua llave y se dirige al palacio, decidido a darle uso.
+        </p>
+      </section>
+    </>
   );
 }
 
