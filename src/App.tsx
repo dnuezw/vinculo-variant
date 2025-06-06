c4dbc2b9d4710b7fc5cf951faf8ac74093f95afe
dnuez <dnuezw@gmail.com>
2025-06-06 18:43:30 +0200

    Add gradient

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,18 @@ const App: FC = () => {
   return (
     <>
       <main className='presentation'>
-        <div className='title'>
-          <div className='logo'>
-            <img className='icon' src={logo} />
-            <img className='text' src={logoText} />
+        <div className='content'>
+          <div className='title'>
+            <div className='logo'>
+              <img className='icon' src={logo} />
+              <img className='text' src={logoText} />
+            </div>
+            <p className='subtitle'>One bufanda one destiny...</p>
           </div>
-          <p className='subtitle'>One bufanda one destiny...</p>
-        </div>
-        <div className='actions'>
-          <button className='trailer'><img src={play} className='playIcon'/>Play trailer</button>
-          <button className='coming-soon'>coming soon</button>
+          <div className='actions'>
+            <button className='trailer'><img src={play} className='playIcon'/>Play trailer</button>
+            <button className='coming-soon'>coming soon</button>
+          </div>
         </div>
       </main>
       <section className='story'>
